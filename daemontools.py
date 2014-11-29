014db50fb88e1f5900bfe404926efaf01aa222fe
Hendrik Morée <hendrik@seecr.nl>
2014-11-29 19:25:28 +0100

    HM: Show back; Show status in menu; Always return to menu

--- daemontools.py
+++ daemontools.py
@@ -1,21 +1,35 @@
 from sublime_plugin import WindowCommand
 from .utils import remoteCommand
-from sublime import set_timeout, message_dialog
+from sublime import set_timeout
 
 class Daemontools(WindowCommand):
 
     def run(self):
         self.view = self.window.active_view()
+        self._listServices()
+
+    def _listServices(self):
         services = remoteCommand(self.view, "ls -1 /etc/service").strip().split('\n')
-        self.window.show_quick_panel(services, lambda i: self._showActions(services[i]) if i != -1 else None)
+        set_timeout(lambda: self.window.show_quick_panel(services, lambda i: self._showActions(services[i]) if i != -1 else None), 0)
 
     def _showActions(self, service):
-        actions = ['start', 'stop', 'restart', 'status']
-        set_timeout(lambda: self.window.show_quick_panel(actions, lambda i: self._doAction(service, actions[i]) if i != -1 else None), 0)
+        actions = ['start', 'stop', 'restart']
+        status = remoteCommand(self.view, "svstat /etc/service/{0}".format(service)).strip()
+
+        def choose(i):
+            if i == -1:
+                return
+            elif i == 0:
+                self._listServices()
+            elif i == 1:
+                self._showActions(service)
+            else:
+                self._doAction(service, actions[i + 1])
+        set_timeout(lambda: self.window.show_quick_panel(["..", status] + actions, choose), 0)
 
     def _doAction(self, service, action):
         if action == 'status':
-            message_dialog(remoteCommand(self.view, "svstat /etc/service/{0}".format(service)).strip())
+            self._showMessage(service, )
             return
         else:
             actionOptions = {
@@ -23,5 +37,8 @@ class Daemontools(WindowCommand):
                 'stop': 'd',
                 'restart': 't'
             }
-        print (remoteCommand(self.view, "svc -{0} /etc/service/{1}".format(actionOptions[action], service)))
+        remoteCommand(self.view, "svc -{0} /etc/service/{1}".format(actionOptions[action], service))
+        self._showActions(service)
 
+    def _showMessage(self, service, message):
+        set_timeout(lambda: self.window.show_quick_panel([message], lambda i: self._showActions(service) if i != -1 else None), 0)
