9c974b3376de8e2f6fe2e69ae5b63b86563efdb7
Hendrik Morée <hendrik@seecr.nl>
2016-05-03 12:20:19 +0200

    HM: Use projectRoot from SublimeUtils

--- utils.py
+++ utils.py
@@ -1,16 +1,9 @@
 from subprocess import Popen, PIPE, TimeoutExpired
 from os.path import dirname, abspath
+from SublimeUtils.sublimeutils import projectRoot
 
 mydir = dirname(abspath(__file__))
 
-def projectRoot(view):
-    currentFile = view.file_name()
-    if currentFile and view.window():
-        for folder in view.window().folders():
-            if folder in currentFile:
-                return folder
-    elif view.window() and view.window().folders():
-        return view.window().folders()[0]
 
 def remoteCommand(view, command):
     rootDir = view.rootDir if hasattr(view, 'rootDir') else projectRoot(view)
